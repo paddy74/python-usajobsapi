e8540460ae3ee197fc384b7dd47ce424b24ef056
Patrick Young <coxpatrick74@gmail.com>
2025-09-10 18:26:04 -0400

    Add pkg _version.py file

--- usajobsapi/__init__.py
+++ usajobsapi/__init__.py
@@ -1,3 +1,5 @@
-"""Top-level package for USAJobs API wrapper."""
+"""Top-level package for the USAJOBS REST API wrapper."""
 
-__all__: list[str] = []
+from usajobsapi._version import __license__, __title__
+
+__all__: list[str] = ["__license__", "__title__"]
