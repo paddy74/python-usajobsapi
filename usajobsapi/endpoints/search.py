7a3baa355e4cc598992d3e7da67fdad8fde0c714
Patrick Young <coxpatrick74@gmail.com>
2025-09-11 13:39:00 -0400

    Add query param fields to SearchEndpoint.Params

--- usajobsapi/endpoints/search.py
+++ usajobsapi/endpoints/search.py
@@ -1,9 +1,9 @@
 """Wrapper for the Job Search API."""
 
 from enum import StrEnum
-from typing import Dict, Optional
+from typing import Annotated, Dict, List, Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
 
 from usajobsapi.utils import _dump_by_alias
 
@@ -67,13 +67,113 @@ class HiringPath(StrEnum):
 # Endpoint declaration
 # ---
 class SearchEndpoint(BaseModel):
-    """Declarative endpoint definition for the Job Search API."""
+    """
+    Declarative endpoint definition for the Job Search API.
+
+    Includes the endpoint's:
+
+    - Parameters
+    - Response shapes
+    - Metadata
+    """
 
     method: str = "GET"
     path: str = "/api/search"
 
     class Params(BaseModel):
-        """Query-params"""
+        """Declarative query-parameter model"""
+
+        model_config = ConfigDict(frozen=True, extra="forbid", populate_by_name=True)
+
+        keyword: Optional[str] = Field(None, serialization_alias="Keyword")
+        position_title: Optional[str] = Field(None, serialization_alias="PositionTitle")
+
+        remuneration_min: Optional[int] = Field(
+            None, serialization_alias="RemunerationMinimumAmount"
+        )
+        remuneration_max: Optional[int] = Field(
+            None, serialization_alias="RemunerationMaximumAmount"
+        )
+        pay_grade_high: Optional[str] = Field(None, serialization_alias="PayGradeHigh")
+        pay_grade_low: Optional[str] = Field(None, serialization_alias="PayGradeLow")
+
+        job_category_codes: List[str] = Field(
+            default_factory=list, serialization_alias="JobCategoryCode"
+        )
+        position_schedule_type_codes: List[str] = Field(
+            default_factory=list, serialization_alias="PositionScheduleTypeCode"
+        )
+        position_offering_type_codes: List[str] = Field(
+            default_factory=list, serialization_alias="PositionOfferingTypeCode"
+        )
+
+        organization: List[str] = Field(
+            default_factory=list, serialization_alias="Organization"
+        )
+        location_names: List[str] = Field(
+            default_factory=list, serialization_alias="LocationName"
+        )
+        radius: Annotated[
+            Optional[int], Field(serialization_alias="Radius", strict=True, gt=0)
+        ] = None
+
+        travel_percentage: List[str] = Field(
+            default_factory=list, serialization_alias="TravelPercentage"
+        )
+        relocation: Optional[bool] = Field(
+            None, serialization_alias="RelocationIndicator"
+        )
+        security_clearance_required: List[str] = Field(
+            default_factory=list, serialization_alias="SecurityClearanceRequired"
+        )
+        position_sensitivity: List[str] = Field(
+            default_factory=list, serialization_alias="PositionSensitivity"
+        )
+
+        who_may_apply: Optional[WhoMayApply] = Field(
+            None, serialization_alias="WhoMayApply"
+        )
+        hiring_paths: List[HiringPath] = Field(
+            default_factory=list, serialization_alias="HiringPath"
+        )
+
+        salary_bucket: List[str] = Field(
+            default_factory=list, serialization_alias="SalaryBucket"
+        )
+        grade_bucket: List[str] = Field(
+            default_factory=list, serialization_alias="GradeBucket"
+        )
+
+        supervisory_status: Optional[str] = Field(
+            None, serialization_alias="SupervisoryStatus"
+        )
+        date_posted_days: Annotated[
+            Optional[int],
+            Field(serialization_alias="DatePosted", strict=True, ge=0, le=60),
+        ] = None
+        job_grade_codes: List[str] = Field(
+            default_factory=list, serialization_alias="JobGradeCode"
+        )
+        mission_critical_tags: List[str] = Field(
+            default_factory=list, serialization_alias="MissionCriticalTags"
+        )
+
+        sort_field: Optional[SortField] = Field(None, serialization_alias="SortField")
+        sort_direction: Optional[SortDirection] = Field(
+            None, serialization_alias="SortDirection"
+        )
+        page: Annotated[
+            Optional[int], Field(serialization_alias="Page", strict=True, ge=1)
+        ] = None
+        results_per_page: Annotated[
+            Optional[int],
+            Field(serialization_alias="ResultsPerPage", strict=True, ge=1, le=500),
+        ] = None
+        fields: Optional[Fields] = Field(None, serialization_alias="Fields")
+
+        remote_indicator: Optional[bool] = Field(
+            None, serialization_alias="RemoteIndicator"
+        )
 
         def to_params(self) -> Dict[str, str]:
             return _dump_by_alias(self)
