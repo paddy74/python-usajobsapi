8b649fb369450cf11ea422fde6b7cd05919eb8b9
Patrick Young <coxpatrick74@gmail.com>
2025-09-11 14:36:04 -0400

    Query params for HistoricJoaEndpoint

--- usajobsapi/endpoints/historicjoa.py
+++ usajobsapi/endpoints/historicjoa.py
@@ -1,8 +1,8 @@
 """Wrapper for the Historic JOAs API."""
 
-from typing import Dict
+from typing import Dict, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, Field
 
 from usajobsapi.utils import _dump_by_alias
 
@@ -23,6 +23,39 @@ class HistoricJoaEndpoint(BaseModel):
 
     class Params(BaseModel):
         """Declarative definition for the endpoint's query parameters."""
+
+        model_config = ConfigDict(frozen=True, extra="forbid", populate_by_name=True)
+
+        hiring_agency_codes: Optional[str] = Field(
+            None, serialization_alias="HiringAgencyCodes"
+        )
+        hiring_department_codes: Optional[str] = Field(
+            None, serialization_alias="HiringDepartmentCodes"
+        )
+        position_series: Optional[str] = Field(
+            None, serialization_alias="PositionSeries"
+        )
+        announcement_numbers: Optional[str] = Field(
+            None, serialization_alias="AnnouncementNumbers"
+        )
+        usajobs_control_numbers: Optional[str] = Field(
+            None, serialization_alias="USAJOBSControlNumbers"
+        )
+        start_position_open_date: Optional[str] = Field(
+            None, serialization_alias="StartPositionOpenDate"
+        )  # YYYY-MM-DD
+        end_position_open_date: Optional[str] = Field(
+            None, serialization_alias="EndPositionOpenDate"
+        )
+        start_position_close_date: Optional[str] = Field(
+            None, serialization_alias="StartPositionCloseDate"
+        )
+        end_position_close_date: Optional[str] = Field(
+            None, serialization_alias="EndPositionCloseDate"
+        )
+        continuation_token: Optional[str] = Field(
+            None, serialization_alias="continuationtoken"
+        )
 
         def to_params(self) -> Dict[str, str]:
             return _dump_by_alias(self)
