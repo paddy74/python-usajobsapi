e5e45894dce21d145629a624aec4300e23b731a3
Patrick Young <coxpatrick74@gmail.com>
2025-09-10 23:17:50 -0400

    Use relative imports in usajobsapi.endpoints

--- usajobsapi/endpoints/__init__.py
+++ usajobsapi/endpoints/__init__.py
@@ -1,8 +1,8 @@
 """Wrapper for USAJOBS REST API endpoints."""
 
-from announcementtext import AnnouncementTextEndpoint
-from historicjoa import HistoricJoaEndpoint
-from search import SearchEndpoint
+from .announcementtext import AnnouncementTextEndpoint
+from .historicjoa import HistoricJoaEndpoint
+from .search import SearchEndpoint
 
 __all__: list[str] = [
     "AnnouncementTextEndpoint",
