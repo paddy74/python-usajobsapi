d6ff07fb0d93fe6c6dfd21b6669cbf1355b9f2e3
Patrick Cox <coxpatrick74@gmail.com>
2025-09-25 19:47:58 -0400

    Add about page to docs

--- mkdocs/hooks.py
+++ mkdocs/hooks.py
@@ -5,5 +5,6 @@ from usajobsapi import _version as md
 def on_config(config: MkDocsConfig):
     config.site_name = f"{md.__title__} {md.__version__}"
     config.site_author = md.__author__
+    config.copyright = f"Copyright &copy; {md.__copyright__}"
 
     return config
