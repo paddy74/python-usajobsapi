499e56d64b7eb290c49aa59463d32dca9255ac17
Patrick Cox <coxpatrick74@gmail.com>
2025-09-25 21:27:50 -0400

    Gen navbar for docstring mkdocs

--- mkdocs/gen_ref_pages.py
+++ mkdocs/gen_ref_pages.py
@@ -6,6 +6,8 @@ import mkdocs_gen_files
 
 
 def gen_ref_pages(root_dir, source_dir, output_dir) -> None:
+    nav = mkdocs_gen_files.Nav()
+
     for path in sorted(source_dir.rglob("*.py")):
         module_path = path.relative_to(source_dir).with_suffix("")
         doc_path = path.relative_to(source_dir).with_suffix(".md")
@@ -17,8 +19,12 @@ def gen_ref_pages(root_dir, source_dir, output_dir) -> None:
             continue
         if module_parts[-1] == "__init__":
             module_parts = module_parts[:-1]
+            doc_path = doc_path.with_name("index.md")
+            full_doc_path = full_doc_path.with_name("index.md")
 
         full_parts = (source_dir.name,) + module_parts
+        nav[full_parts] = doc_path.as_posix()
+
         if not module_parts:
             identifier = source_dir.name
         else:
@@ -28,6 +34,9 @@ def gen_ref_pages(root_dir, source_dir, output_dir) -> None:
             print("::: " + identifier, file=fd)
 
         mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root_dir))
+
+    with mkdocs_gen_files.open("ref/NAV_REF.md", "w") as nav_file:
+        nav_file.writelines(nav.build_literate_nav())
 
 
 root = Path(__file__).parent.parent
