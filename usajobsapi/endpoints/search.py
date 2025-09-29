af6bf0f44be571acec23a7faabc8ba5c2042c35a
Patrick Cox <coxpatrick74@gmail.com>
2025-09-29 15:21:29 -0400

    Add desc and validators to SearchEndpoint.Params

--- usajobsapi/endpoints/search.py
+++ usajobsapi/endpoints/search.py
@@ -6,6 +6,7 @@ from enum import StrEnum
 from typing import Annotated, Any, Dict, List, Optional
 
 from pydantic import (
+    AfterValidator,
     BaseModel,
     ConfigDict,
     Field,
@@ -15,6 +16,7 @@ from pydantic import (
     model_validator,
 )
 
+from usajobsapi.endpoints._validators import isvalid_pay_grade
 from usajobsapi.utils import _dump_by_alias
 
 # Enums for query-params
@@ -55,7 +57,7 @@ class WhoMayApply(StrEnum):
     STATUS = "Status"
 
 
-class Fields(StrEnum):
+class FieldsMinMax(StrEnum):
     """Return the minimum or maximum number of fields for each result item."""
 
     MIN = "Min"  # Return only the job summary
@@ -99,94 +101,268 @@ class SearchEndpoint(BaseModel):
 
         model_config = ConfigDict(frozen=True, extra="forbid", populate_by_name=True)
 
-        keyword: Optional[str] = Field(None, serialization_alias="Keyword")
-        position_title: Optional[str] = Field(None, serialization_alias="PositionTitle")
+        keyword: Optional[str] = Field(
+            None,
+            serialization_alias="Keyword",
+            description="Issues search to find hits based on a keyword. Optional. Keyword will search for all of the words specified (or synonyms of the word) throughout the job announcement.",
+            examples=["https://data.usajobs.gov/api/search?Keyword=Software"],
+        )
+        position_title: Optional[str] = Field(
+            None,
+            serialization_alias="PositionTitle",
+            description="""
+            Issues search to find hits in the title of the job.
+
+            This is the job title - e.g. IT Specialist, Psychologist, etc. The title search will be treated as 'contains' and will select all job announcements where the job title contains the value provided.""",
+            examples=[
+                "The following query will return all job announcements with 'psychologist' or a synonym of psychologist in the title: 'https://data.usajobs.gov/api/Search?PositionTitle=Psychologist'",
+                "The following query will return all job announcements with 'Electrical Engineer'' in the title: 'https://data.usajobs.gov/api/Search?PositionTitle=Electrical%20Engineer'",
+            ],
+        )
 
         remuneration_min: Optional[int] = Field(
-            None, serialization_alias="RemunerationMinimumAmount"
+            None,
+            serialization_alias="RemunerationMinimumAmount",
+            description="""
+            Issues search to find hits with the minimum salary specified.
+
+            Jobs are placed in salary buckets: $0-$24,999, $25,000-$49,999, $50,000-$74,999, $75,000-$99,999, $100,000-$124,999, $125,000-$149,999, $150,000-$174,999, $175,000-$199,999 and $200,000 or greater. So a search with a minimum salary of $15,500 will return jobs with a minimum salary in the $0-$24,999 range.""",
+            examples=[
+                "https://data.usajobs.gov/api/Search?RemunerationMinimumAmount=15000"
+            ],
+            ge=0,
         )
         remuneration_max: Optional[int] = Field(
-            None, serialization_alias="RemunerationMaximumAmount"
+            None,
+            serialization_alias="RemunerationMaximumAmount",
+            description="""
+            Issues search to find hits with the maximum salary specified.
+
+            Jobs are placed in salary buckets: $0-$24,999, $25,000-$49,999, $50,000-$74,999, $75,000-$99,999, $100,000-$124,999, $125,000-$149,999, $150,000-$174,999, $175,000-$199,999 and $200,000 or greater. So a search with a maximum salary of $72,000 will return jobs with a maximum salary in the $50,000-$74,999 range.
+            """,
+            examples=[
+                "https://data.usajobs.gov/api/Search?RemunerationMinimumAmount=26000&RemunerationMaximumAmount=85000"
+            ],
+            gt=0,
         )
-        pay_grade_high: Optional[str] = Field(None, serialization_alias="PayGradeHigh")
-        pay_grade_low: Optional[str] = Field(None, serialization_alias="PayGradeLow")
+
+        pay_grade_high: Annotated[Optional[str], AfterValidator(isvalid_pay_grade)] = (
+            Field(
+                None,
+                serialization_alias="PayGradeHigh",
+                description="""
+                Issues search to find hits with the maximum pay grade specified. Must be 01 through 15. This is the ending grade for the job. (Caution: Fed speak ahead but it cannot be helped.) The grade along with series is used by the Federal government to categorize and define jobs.
+
+                For more information on what series and grade are, please visit: https://help.usajobs.gov/index.php/What_is_a_series_and_or_grade%3F.
+
+                However, grade is also used to define salary. USAJOBS search uses grades for the General Schedule (GS) pay plan ( http://www.opm.gov/policy-data-oversight/pay-leave/salaries-wages).
+
+                For jobs that use a different pay plan than the GS schedule, USAJOBS will derive the corresponding grade by using the minimum and maximum salary and the wages for the GS schedule for the Rest of the United States (for 2014, see: http//www.opm.gov/policy-data-oversight/pay-leave/salaries-wages/salary-tables/14Tables/html/RUS.aspx).
+
+                For federal employees, especially those who have a GS pay plan, searching by grade is extremely useful since they would already know which grades they are or qualify for. However, for non-GS employees, searching by salary is much simpler.
+                """,
+                examples=["https://data.usajobs.gov/api/Search?PayGradeHigh=07"],
+            )
+        )
+        pay_grade_low: Annotated[Optional[str], AfterValidator(isvalid_pay_grade)] = (
+            Field(
+                None,
+                serialization_alias="PayGradeLow",
+                description="Issues search to find hits with the minimum pay grade specified. Must be 01 through 15. This is the beginning grade for the job. See PayGradeHigh for more information.",
+                examples=[
+                    "https://data.usajobs.gov/api/Search?PayGradeLow=04",
+                    "https://data.usajobs.gov/api/Search?PayGradeLow=07&PayGradeHigh=09",
+                ],
+            )
+        )
 
         job_category_codes: List[str] = Field(
-            default_factory=list, serialization_alias="JobCategoryCode"
+            default_factory=list,
+            serialization_alias="JobCategoryCode",
+            description="Issues a search to find hits with the job category series specified.",
+            examples=["https://data.usajobs.gov/api/Search?JobCategoryCode=0830"],
         )
-        position_schedule_type_codes: List[str] = Field(
-            default_factory=list, serialization_alias="PositionScheduleTypeCode"
+        position_schedule_type_codes: List[int] = Field(
+            default_factory=list,
+            serialization_alias="PositionScheduleTypeCode",
+            description="Issues a search to find hits for jobs matching the specified job schedule. This field is also known as work schedule.",
+            examples=["https://data.usajobs.gov/api/Search?PositionSchedule=4"],
         )
-        position_offering_type_codes: List[str] = Field(
-            default_factory=list, serialization_alias="PositionOfferingTypeCode"
+        position_offering_type_codes: List[int] = Field(
+            default_factory=list,
+            serialization_alias="PositionOfferingTypeCode",
+            description="Issues a search to find jobs within the specified type. This field is also known as Work Type.",
         )
 
         organization: List[str] = Field(
-            default_factory=list, serialization_alias="Organization"
+            default_factory=list,
+            serialization_alias="Organization",
+            description="Issues a search to find jobs for the specified agency using the Agency Subelement Code.",
+            examples=["https://data.usajobs.gov/api/Search?Organization=TR"],
         )
-        location_names: List[str] = Field(
-            default_factory=list, serialization_alias="LocationName"
-        )
-        radius: Annotated[
-            Optional[int], Field(serialization_alias="Radius", strict=True, gt=0)
-        ] = None
 
-        travel_percentage: List[str] = Field(
-            default_factory=list, serialization_alias="TravelPercentage"
+        location_names: List[str] = Field(
+            default_factory=list,
+            serialization_alias="LocationName",
+            description="Issues a search to find hits within the specified location. This is the city or military installation name. LocationName simplifies location based search as the user does not need to know or account for each and every Location Code. LocationName will search for all location codes and ZIP codes that have that specific description.",
+            examples=[
+                "https://data.usajobs.gov/api/Search?LocationName=Washington%20DC,%20District%20of%20Columbia",
+                "https://data.usajobs.gov/api/Search?LocationName=Atlanta,%20Georgia",
+            ],
+        )
+
+        travel_percentage: List[int] = Field(
+            default_factory=list,
+            serialization_alias="TravelPercentage",
+            description="Issues a search to find hits for jobs matching the specified travel level.",
+            examples=[
+                "https://data.usajobs.gov/api/Search?TravelPercentage=0",
+                "https://data.usajobs.gov/api/Search?TravelPercentage=7",
+            ],
+            ge=0,
+            le=8,
         )
         relocation: Optional[bool] = Field(
-            None, serialization_alias="RelocationIndicator"
+            None,
+            serialization_alias="RelocationIndicator",
+            description="Issues a search to find hits for jobs matching the relocation filter.",
         )
-        security_clearance_required: List[str] = Field(
-            default_factory=list, serialization_alias="SecurityClearanceRequired"
-        )
-        position_sensitivity: List[str] = Field(
-            default_factory=list, serialization_alias="PositionSensitivity"
-        )
-
-        who_may_apply: Optional[WhoMayApply] = Field(
-            None, serialization_alias="WhoMayApply"
-        )
-        hiring_paths: List[HiringPath] = Field(
-            default_factory=list, serialization_alias="HiringPath"
-        )
-
-        salary_bucket: List[str] = Field(
-            default_factory=list, serialization_alias="SalaryBucket"
-        )
-        grade_bucket: List[str] = Field(
-            default_factory=list, serialization_alias="GradeBucket"
+        security_clearance_required: List[int] = Field(
+            default_factory=list,
+            serialization_alias="SecurityClearanceRequired",
+            description="Issues a search to find hits for jobs matching the specified security clearance.",
+            examples=[
+                "https://data.usajobs.gov/api/Search?SecurityClearanceRequired=1"
+            ],
+            ge=0,
+            le=8,
         )
 
         supervisory_status: Optional[str] = Field(
-            None, serialization_alias="SupervisoryStatus"
-        )
-        date_posted_days: Annotated[
-            Optional[int],
-            Field(serialization_alias="DatePosted", strict=True, ge=0, le=60),
-        ] = None
-        job_grade_codes: List[str] = Field(
-            default_factory=list, serialization_alias="JobGradeCode"
-        )
-        mission_critical_tags: List[str] = Field(
-            default_factory=list, serialization_alias="MissionCriticalTags"
+            None,
+            serialization_alias="SupervisoryStatus",
+            description="Issues a search to find hits for jobs matching the specified supervisory status.",
         )
 
-        sort_field: Optional[SortField] = Field(None, serialization_alias="SortField")
-        sort_direction: Optional[SortDirection] = Field(
-            None, serialization_alias="SortDirection"
+        days_since_posted: Annotated[
+            Optional[int],
+            Field(
+                serialization_alias="DatePosted",
+                description="Issues a search to find hits for jobs that were posted within the number of days specified.",
+                strict=True,
+                ge=0,
+                le=60,
+            ),
+        ] = None
+        job_grade_codes: List[str] = Field(
+            default_factory=list,
+            serialization_alias="JobGradeCode",
+            description="Issues a search to find hits for jobs matching the grade code specified. This field is also known as Pay Plan.",
         )
+
+        sort_field: Optional[SortField] = Field(
+            None,
+            serialization_alias="SortField",
+            description="Issues a search that will be sorted by the specified field.",
+            examples=[
+                "https://data.usajobs.gov/api/Search?PositionTitle=Electrical&SortField=PositionTitle"
+            ],
+        )
+        sort_direction: Optional[SortDirection] = Field(
+            None,
+            serialization_alias="SortDirection",
+            description="Issues a search that will be sorted by the SortField specified, in the direction specified.",
+        )
+
         page: Annotated[
-            Optional[int], Field(serialization_alias="Page", strict=True, ge=1)
+            Optional[int],
+            Field(
+                serialization_alias="Page",
+                description="Issues a search to pull the paged results specified.",
+                strict=True,
+                ge=1,
+            ),
         ] = None
         results_per_page: Annotated[
             Optional[int],
-            Field(serialization_alias="ResultsPerPage", strict=True, ge=1, le=500),
+            Field(
+                serialization_alias="ResultsPerPage",
+                description="Issues a search and returns the page size specified. In this example, 25 jobs will be return for the first page.",
+                strict=True,
+                ge=1,
+                le=500,
+            ),
         ] = None
-        fields: Optional[Fields] = Field(None, serialization_alias="Fields")
+
+        who_may_apply: Optional[WhoMayApply] = Field(
+            None,
+            serialization_alias="WhoMayApply",
+            description="Issues a search to find hits based on the desired candidate designation. In this case, public will find jobs that U.S. citizens can apply for.",
+            examples=["https://data.usajobs.gov/api/Search?WhoMayApply=public"],
+        )
+
+        radius: Annotated[
+            Optional[int],
+            Field(
+                serialization_alias="Radius",
+                description="Issues a search when used along with LocationName, will expand the locations, based on the radius specified.",
+                examples=[
+                    "https://data.usajobs.gov/api/Search?LocationName=Norfolk%20Virginia&Radius=75"
+                ],
+                strict=True,
+                gt=0,
+            ),
+        ] = None
+        fields: Optional[FieldsMinMax] = Field(
+            None,
+            serialization_alias="Fields",
+            description="Issues a search that will return the minimum fields or maximum number of fields in the job. Min returns only the job summary.",
+            examples=[
+                "https://data.usajobs.gov/api/Search?TravelPercentage=7&Fields=full",
+                "https://data.usajobs.gov/api/Search?SecurityClearanceRequired=1&Fields=full",
+            ],
+        )
+
+        salary_bucket: List[int] = Field(
+            default_factory=list,
+            serialization_alias="SalaryBucket",
+            description="Issues a search that will find hits for salaries matching the grouping specified. Buckets are assigned based on salary ranges.",
+        )
+        grade_bucket: List[int] = Field(
+            default_factory=list,
+            serialization_alias="GradeBucket",
+            description="Issues a search that will find hits for grades that match the grouping specified.",
+        )
+
+        hiring_paths: List[HiringPath] = Field(
+            default_factory=list,
+            serialization_alias="HiringPath",
+            description="Issues a search that will find hits for hiring paths that match the hiring paths specified.",
+            examples=["https://data.usajobs.gov/api/Search?HiringPath=public"],
+        )
+
+        mission_critical_tags: List[str] = Field(
+            default_factory=list,
+            serialization_alias="MissionCriticalTags",
+            description="Issues a search that will find hits for mission critical tags that match the grouping specified.",
+            examples=[
+                "https://data.usajobs.gov/api/Search?MissionCriticalTags=STEM&Fields=full"
+            ],
+        )
+
+        position_sensitivity: List[int] = Field(
+            default_factory=list,
+            serialization_alias="PositionSensitivity",
+            description="Issues a search that will find hits for jobs matching the position sensitivity and risk specified.",
+            examples=["https://data.usajobs.gov/api/Search?PositionSensitivity=1"],
+            ge=1,
+            le=7,
+        )
 
         remote_indicator: Optional[bool] = Field(
-            None, serialization_alias="RemoteIndicator"
+            None,
+            serialization_alias="RemoteIndicator",
+            description="Issues a search to find hits for jobs matching the remote filter.",
         )
 
         @model_validator(mode="after")
@@ -405,14 +581,17 @@ class SearchEndpoint(BaseModel):
         result_count: Optional[int] = Field(
             default=None,
             alias="SearchResultCount",
+            description="Number of records returned in response object.",
         )
         result_total: Optional[int] = Field(
             default=None,
             alias="SearchResultCountAll",
+            description="Total Number of records that matched search criteria.",
         )
         items: List[Dict[str, Any]] = Field(
             default_factory=list,
             alias="SearchResultItems",
+            description="Array of job opportunity announcement objects that matched search criteria.",
         )
 
         def jobs(self) -> List[SearchEndpoint.JobSummary]:
@@ -430,10 +609,15 @@ class SearchEndpoint(BaseModel):
     class Response(BaseModel):
         """Declarative definition of the endpoint's response object."""
 
-        language: Optional[str] = Field(default=None, alias="LanguageCode")
-        params: Optional[SearchEndpoint.Params] = Field(
-            default=None, alias="SearchParameters"
+        language: Optional[str] = Field(
+            default=None, alias="LanguageCode", description="Response Langauge"
         )
+        params: Optional[SearchEndpoint.Params] = Field(
+            default=None,
+            alias="SearchParameters",
+            description="Query parameters used in search request.",
+        )
+
         # Results are wrapped under SearchResult
         search_result: Optional[SearchEndpoint.SearchResult] = Field(
             default=None, alias="SearchResult"
